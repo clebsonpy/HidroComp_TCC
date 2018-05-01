725ab3bf8b9abb5c3280e4af03c492c83f49c659
Clebson Carvalho <Clebson2007.farias@gmail.com>
2018-05-01 10:26:27 -0300

    Site in english

--- parcial/forms.py
+++ parcial/forms.py
@@ -3,12 +3,12 @@ from django import forms
 
 class ParcialForm(forms.Form):
 
-    station = forms.CharField(label='Estação', max_length=25)
-    font = forms.CharField(label='Fonte', max_length=25)
-    date_start = forms.DateField(label='Data Início', required=False)
-    date_end = forms.DateField(label='Data Fim', required=False)
-    type_threshold = forms.CharField(label='Tipo Limiar', max_length=25)
-    value_threshold = forms.FloatField(label='Valor Limiar')
-    type_criterion = forms.CharField(label='Critério', max_length='50')
-    type_event = forms.CharField(label='Evento', max_length=25)
-    duration = forms.IntegerField(label='Duração entre eventos', required=False)
+    station = forms.CharField(label='Station', max_length=25)
+    font = forms.CharField(label='Font', max_length=25)
+    date_start = forms.DateField(label='Start Date', required=False)
+    date_end = forms.DateField(label='End Start', required=False)
+    type_threshold = forms.CharField(label='Threshold type', max_length=25)
+    value_threshold = forms.FloatField(label='Threshold Value')
+    type_criterion = forms.CharField(label='Criterion', max_length='50')
+    type_event = forms.CharField(label='Event', max_length=25)
+    duration = forms.IntegerField(label='Duration Eetween Events', required=False)
