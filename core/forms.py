d60123d76e533a0c678264f373e94b656af5bcfa
Clebson Carvalho <Clebson2007.farias@gmail.com>
2018-05-01 11:42:06 -0300

    falta finalizar Odm2Core

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 
-from odm2admin.models import Results, Units, Variables
-from odm2admin.forms import (UnitsAdminForm, ResultsAdminForm, VariablesAdminForm)
+from odm2admin.models import Results, Units, Variables, Processinglevels
+from odm2admin.forms import (UnitsAdminForm, ResultsAdminForm, VariablesAdminForm,
+                             ProcessingLevelsAdminForm)
 
 from betterforms.multiform import MultiModelForm
 
@@ -10,7 +11,8 @@ class ResultsForm(ResultsAdminForm):
 
     class Meta:
         model = Results
-        exclude = ['unitsid', 'variableid']
+        exclude = ['unitsid', 'variableid', 'featureactionid', 'taxonomicclassifierid',
+                   'statuscv', 'processing_level']
 
 class VariablesForm(VariablesAdminForm):
 
@@ -26,21 +28,30 @@ class UnitsForm(UnitsAdminForm):
         exclude = ['unitslink']
 
 
+class ProcessinglevelsForm(ProcessingLevelsAdminForm):
+
+    class Meta:
+        model = Processinglevels
+        fields = ['processinglevelcode']
+
+
 class ResultsMultiForm(MultiModelForm):
 
-    form_classes = {'results': ResultsForm,
-                    'units': UnitsForm,
-                    'variables': VariablesForm}
+    form_classes = {'results': ResultsForm, 'units': UnitsForm,
+                    'variables': VariablesForm, 'processing': ProcessinglevelsForm}
 
     def save(self, commit=True):
         objects = super(ResultsMultiForm, self).save(commit=False)
 
         if commit:
+            processing = objects['processing']
+            processing.save()
             units = objects['units']
             units.save()
             variables = objects['variables']
             variables.save()
             results = objects['results']
+            results.processing = processing
             results.units = units
             results.variables = variables
             results.save()
